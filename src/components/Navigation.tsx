6a7fdf00b0aad875429c516bfa7259868be73e59
Abhishek katroliya <56072322+Abhishekkat@users.noreply.github.com>
2024-12-20 14:15:35 +0530

    Create Navigation.tsx

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,25 @@
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import Navigation from "@/components/Navigation";
+
+const Index = () => {
+  return (
+    <main className="bg-navy">
+      <Navigation />
+      <Hero />
+      <About />
+      <Experience />
+      <Education />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+};
+
+export default Index;
